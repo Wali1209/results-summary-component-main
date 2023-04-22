e54a090ee1da0680f4bf9f5bc4e43888ba55c1fa
wali1209 <walibilal1209@gmail.com>
2023-04-23 00:24:35 +0500

    Global Styles added!

--- src/Components/Styles/Global.styled.tsx
+++ src/Components/Styles/Global.styled.tsx
@@ -1,5 +1,33 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    
+@font-face {
+  font-family: 'MyFont';
+  src: url("../../../assets/fonts/HankenGrotesk-VariableFont_wght.ttf")  format('truetype');
+}
+html{
+    font-size: 16px; //setting base font size 
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+/* Global font styles */
+body {
+  font-family: "MyFont", sans-serif;
+  line-height: 1.5;
+  font-weight: normal;
+}
+
+/* Global color styles */
+body {
+  color: #333;
+  background-color: #f8f8f8;
+}
+p{
+    font-size: 1.125rem;
+}
 `;
