4213db059258b532b3e663b848ec1e182f8e2d7c
wali1209 <walibilal1209@gmail.com>
2023-04-24 14:35:29 +0500

    base line added! in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/results-summary-component-main/', //Adding base is part of deploying app to gh-pages
   plugins: [react()],
 })
